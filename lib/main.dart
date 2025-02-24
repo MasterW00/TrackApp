7f12ac911fa2c9ac77afb9e44d74344b35367241
Jared Farnbach <jaredfarnbach@gmail.com>
2025-02-24 04:28:59 -0700

    Database storage and users

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:mongo_dart/mongo_dart.dart' as mongo ;
 
 void main() {
   runApp(const MyApp());
@@ -41,23 +42,110 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier{
-  GlobalKey<FormState> unit = GlobalKey<FormState>();
-  GlobalKey<FormState> taskForm = GlobalKey<FormState>();
-  List<Reminder> reminders = [];
+  var username = '\r';
+  var db;
+  //connect to database
+  Future<void> connect() async{  
+    db = await mongo.Db.create("mongodb+srv://jaredfarnbach:mnbvcxz12@ground.7yok2.mongodb.net/TrackUsers?retryWrites=true&w=majority");
+    await db.open();
+    print('Connected to the database');
+  }
+  //createing user
+  Future<void> enrollNewUser(String user, String password) async{
+    if(db == null){
+      await connect();
+    }
+    int id = user.hashCode;
+    await db.collection('users').insert({'_id':id,'username': user,'password': password.hashCode});
+    username = user; 
+    notifyListeners();
+  }
+  Future<void> loadUserData() async{
+    if(db == null){
+      return;
+    }
+    var userData = await db.collection('user_data').findOne({'username': username});
+    if (userData != null){
+      tracks = (userData['tracks'] as List).map((track) => Reminder.fromMap(track)).toList();
+    }
+    notifyListeners();
+  }
+  //logging in
+  Future<void> login(String user, String password) async{
+    if(username != '\r'){
+      return;
+    }
+    if(db == null){
+      await connect();
+    }
+    var auth = await db.collection('users').findOne({'username': user});
+    if (auth != null) {
+      if (auth['password'] == password.hashCode) {
+        print('password matches');
+        username = user;
+      } 
+      else {
+        print('password does not match');
+        username = '\r';
+      }
+    } 
+    else {
+      print('user not found');
+    }
+    notifyListeners();
+  }
+
+  //tracks and reminders
+  List<Reminder> tracks = [];
+  Future <void> saveTrackInfo() async{
+    if(db == null){
+      return;
+    }
+    if (tracks.isNotEmpty){
+      await db.collection('user_data').update(
+        {'username': username},
+        {
+          '\$set': {
+            'tracks': tracks.map((track) => track.toMap()).toList(),
+          }
+        },
+        upsert: true,
+       );
+    }
+  }
+  Future<void> deleteUser() async{
+    if(db == null){
+      return;
+    }
+    await db.collection('users').remove({'username': username});
+    await db.collection('user_data').remove({'username': username});
+    username = '\r';
+    tracks = [];
+    notifyListeners();
+  }
   void newReminder(Reminder newReminder){
-    reminders.add(newReminder);
+    tracks.add(newReminder);
     notifyListeners();
   }
   void addTask(Reminder currentReminder, Task newestTask){
     currentReminder.addTask(newestTask);
     notifyListeners();
   }
-  void removeTask(Reminder reminder, Task task){
+  Future<void> removeTask(Reminder reminder, Task task) async{
     reminder.tasks.remove(task);
+    // await db.collection('user_data').update(
+    //   {'username': username},
+    //   {
+    //     '\$set': {
+    //       'tracks': tracks.map((track) => track.toMap()).toList(),
+    //     }
+    //   },
+    //   upsert: true,
+    // );
     notifyListeners();
   }
   Reminder? getReminder(Reminder reminder){
-    for(Reminder currentReminder in reminders){
+    for(Reminder currentReminder in tracks){
       if (reminder == currentReminder){
         return currentReminder;
       }
@@ -84,8 +172,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Widget page = CurrentReminders(turner: ({Widget? page}){});
-  bool home = true;
+  //Widget page = CurrentReminders(turner: ({Widget? page}){});
+  Widget page = Login(enroll: true);  
+  bool home = false;
 
   void _turnPage({Widget? page}) {
     setState(() {
@@ -112,6 +201,7 @@ class _MyHomePageState extends State<MyHomePage> {
   
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -134,10 +224,17 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        leading: IconButton(
+          icon: Icon(Icons.person_rounded),
+          onPressed: (){_turnPage(page:Login(enroll: false));},
+        ),
       ),
       body: page,
       floatingActionButton: FloatingActionButton(
-        onPressed:(){_turnPage();},
+        onPressed:(){
+            appState.saveTrackInfo();
+            _turnPage();
+          },
         tooltip: 'Add',
         child: Icon(icon),
       ), // This trailing comma makes auto-formatting nicer for build methods.
@@ -145,6 +242,160 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+//login page
+class Login extends StatefulWidget {
+  final bool? enroll;
+  const Login({super.key, this.enroll});
+  
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  late String username;
+  late String password;
+  late Widget formPrompt;
+  @override 
+  initState(){
+    super.initState();
+    formPrompt = widget.enroll == true ? Text('Create New Account') : Text('Welcome back');
+  }
+  @override
+  void didUpdateWidget(Login oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.enroll != widget.enroll) {
+      setState(() {
+        formPrompt = widget.enroll == true ? Text('Create New Account') : Text('Welcome Back');
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    GlobalKey<FormState> userForm = GlobalKey<FormState>();
+    var appState = context.watch<MyAppState>();
+    var username = appState.username;
+    if(username != '\r'){
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text('Welcome $username', style: Theme.of(context).textTheme.headlineMedium),
+            ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: (){
+                        appState.username = '\r';
+                        appState.tracks = [];
+                        setState(() {
+                          formPrompt = widget.enroll == true ? Text('Create New Account') : Text('Login');
+                        });
+                      },
+                      child: Text('Logout'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: (){
+                        appState.deleteUser();
+                        setState(() {
+                          formPrompt = widget.enroll == true ? Text('Create New Account') : Text('Login');
+                        });
+                      },
+                      child: Text('Delete Account'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+    return Form(
+        key: userForm,
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              formPrompt,
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    hintText: 'Username',
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a username';
+                    }
+                    return null;
+                  },
+                  onSaved: (String? newValue){
+                    username = newValue!;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    hintText: 'password',
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    return null;
+                  },
+                  onSaved: (String? newValue){
+                    password = newValue!;
+                  },
+                ),
+              ),
+              ElevatedButton(
+                onPressed: ()async{
+                  if (userForm.currentState!.validate()) {
+                    userForm.currentState!.save();
+                    setState(() {
+                      if(appState.username == '\r'){
+                        formPrompt = Text('Logging in . . .');
+                      }
+                    });
+                    if(widget.enroll == true){
+                      await appState.enrollNewUser(username, password);
+                    }
+                    else{
+                      await appState.login(username, password);
+                    }
+                    if(appState.username != '\r'){
+                      await appState.loadUserData();
+                    }
+                  }
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
+                ),
+        ),
+    );
+  }
+}
+
+//track start page
 class Creator extends StatelessWidget{
   final Function({Widget? page}) turner;
   const Creator({super.key, required this.turner});
@@ -152,7 +403,7 @@ class Creator extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    var unit = appState.unit;
+    GlobalKey<FormState> unit = GlobalKey<FormState>();
     Reminder newestReminder;
     return Center(
       child: Form(
@@ -179,7 +430,7 @@ class Creator extends StatelessWidget{
                           if (value == null || value.isEmpty) {
                             return 'Needs a name';
                           }
-                          for(Reminder one in appState.reminders){
+                          for(Reminder one in appState.tracks){
                             if(value == one.name){
                               return 'Item already exists';
                             }
@@ -242,7 +493,7 @@ class CurrentReminders extends StatelessWidget {
           Center(
             child: Column(
               children: [
-                for(var reminder in appState.reminders)
+                for(var reminder in appState.tracks)
                 ListTile(
                   tileColor: Theme.of(context).colorScheme.primaryContainer,
                   onTap:(){
@@ -265,21 +516,38 @@ class CurrentReminders extends StatelessWidget {
 
 //reminder object structure
 class Reminder {
+  late mongo.ObjectId id;
   late final String name;
   List<Task> tasks;
-  Reminder({required this.name}): tasks = [];
+
+  Reminder({required this.name}): tasks = [], id = mongo.ObjectId();
 
   void addTask(Task newTask){
     tasks.add(newTask);
   }
+  Map<String, dynamic> toMap(){
+    return {
+      '_id' : id,
+      'name': name,
+      'tasks': tasks.map((task) => task.toMap()).toList(),
+    };
+  }
+  factory Reminder.fromMap(Map<String, dynamic> map){
+    return Reminder(name: map['name'] as String)
+      ..id = map['_id'] as mongo.ObjectId
+      ..tasks = (map['tasks'] as List).map((task) => Task.fromMap(task)).toList();
+  }
 }
 
 class Task{
+  late mongo.ObjectId id;
   late final String name;
-  late final Map<String, Object> recurrence;
+  late final Map<String, dynamic> recurrence;
   late DateTime nextOccurrence;
   late DateTime lastOccurrence;
-  Task({required this.name});
+  
+  Task({required this.name}):id = mongo.ObjectId();
+
   double calculateProgress(){
     final now = DateTime.now();
     final totalDuration = nextOccurrence.difference(lastOccurrence).inDays;
@@ -321,8 +589,24 @@ class Task{
         return nextOccurrence;
     }
   }
-}
 
+  Map<String, dynamic> toMap(){
+    return {
+      '_id': id,
+      'name': name,
+      'recurrence': recurrence,
+      'nextOccurrence': nextOccurrence.toIso8601String(),
+      'lastOccurrence': lastOccurrence.toIso8601String(),
+    };
+  }
+  factory Task.fromMap(Map<String, dynamic> map){
+    return Task(name: map['name'] as String)
+      ..id = map['_id'] as mongo.ObjectId
+      ..recurrence = map['recurrence'] as Map<String, dynamic>
+      ..nextOccurrence = DateTime.parse(map['nextOccurrence'] as String)
+      ..lastOccurrence = DateTime.parse(map['lastOccurrence'] as String);
+  } 
+}
 //Task view page
 class TaskList extends StatefulWidget{
   final Reminder reminder;
@@ -336,11 +620,12 @@ class _TaskListState extends State<TaskList> {
   late var recurrence = {'frequency': 0, 'period': ''};
   late Task newestTask;
   late DateTime startDate;
+  
   @override
   Widget build(BuildContext context) {
+    //var appState = context.watch<MyAppState>();
+    GlobalKey<FormState> taskForm = GlobalKey<FormState>();
     var appState = context.watch<MyAppState>();
-    var key = appState.taskForm;
-    
     return ListView(
       children: [
         Center(
@@ -354,7 +639,8 @@ class _TaskListState extends State<TaskList> {
             ListTile(
               leading: IconButton(
                 onPressed: ()=> setState(() {
-                    widget.reminder.tasks.remove(task);
+                  appState.removeTask(widget.reminder, task);
+                  //widget.reminder.tasks.remove(task);
                 }),
                 icon: Icon(Icons.close_rounded),
               ),
@@ -382,7 +668,7 @@ class _TaskListState extends State<TaskList> {
               height: 1,
             ),
             Form(
-              key: key,
+              key: taskForm,
               child: Center(
                 child: Column(
                   children:[
@@ -509,8 +795,8 @@ class _TaskListState extends State<TaskList> {
                         backgroundColor: Theme.of(context).colorScheme.onSecondary,
                       ),
                       onPressed: (){
-                        if (key.currentState!.validate()) {
-                          key.currentState!.save();
+                        if (taskForm.currentState!.validate()) {
+                          taskForm.currentState!.save();
                           setState((){
                             newestTask.recurrence = recurrence;
                             newestTask.nextOccurrence = startDate;
